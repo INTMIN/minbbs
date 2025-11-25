20608b9c8596e16c0a6d9d69eeea5b2bfeaf1d6c
INTMIN <min_world@qq.com>
2025-11-25 17:40:08 +0800

    feat: 添加液态玻璃盒子

--- app/components/MouseLiquidGrass/index.tsx
+++ app/components/MouseLiquidGrass/index.tsx
@@ -1,3 +1,4 @@
+import { Card } from "@heroui/react";
 import React from "react";
 import LiquidGlass from "~/components/LiquidGrass";
 
@@ -19,7 +20,25 @@ const MouseLiquidGrass = () => {
         touchAction: "none",
       }}
     >
-      <LiquidGlass mode="standard">可拖拽的液态玻璃</LiquidGlass>
+      {/* <LiquidGlass mode="standard" className="z-10" blurAmount={0.1}>
+        可拖拽的液态玻璃
+      </LiquidGlass> */}
+      <LiquidGlass
+        displacementScale={100}
+        blurAmount={0.08}
+        saturation={180}
+        elasticity={0.2}
+        mode="prominent"
+        cornerRadius={24}
+        className="z-10"
+      >
+        <Card className="bg-transparent">
+          <div className="p-6 text-[#b3b3b3]">
+            <p>这里有个液态玻璃盒子</p>
+            <p>你可以拖动他查看效果</p>
+          </div>
+        </Card>
+      </LiquidGlass>
     </div>
   );
 };
